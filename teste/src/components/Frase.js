f4825adc858133e64c68fd527517edb7b3ad1869
Notsume <adahorapirola@gmail.com>
2024-01-31 13:57:44 -0300

    Inserindo CSS no React

--- teste/src/components/Frase.js
+++ teste/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css';
+
 function Frase () {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolor doloremque corrupti nulla sint consequuntur, ut, quod debitis ipsa odit enim quas, animi incidunt excepturi sunt beatae dignissimos ullam quidem.</p>
+        <div className={styles.frase_container}>
+            <p className={styles.fraseContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolor doloremque corrupti nulla sint consequuntur, ut, quod debitis ipsa odit enim quas, animi incidunt excepturi sunt beatae dignissimos ullam quidem.</p>
         </div>
     )
 }
